2a131ec4afe42d5df1cb4421b35750562661bc1a
shantanu <shantanumapari17@gmail.com>
2022-10-18 00:40:13 +0530

    loaded static

--- StudyMate/StudyMate/settings.py
+++ StudyMate/StudyMate/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "study.apps.StudyConfig",
 ]
 
 MIDDLEWARE = [
@@ -111,6 +112,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR,'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 

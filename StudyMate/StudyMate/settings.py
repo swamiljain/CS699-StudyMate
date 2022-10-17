59ccde4b0ccb5f38901a51bf98ea835305c49838
shantanu <shantanumapari17@gmail.com>
2022-10-18 01:44:09 +0530

    added account app , and managed static and media files

--- StudyMate/StudyMate/settings.py
+++ StudyMate/StudyMate/settings.py
@@ -120,4 +120,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+# Base url to serve media files
+MEDIA_URL = '/media/'
+
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
